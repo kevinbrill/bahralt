e13fba154409304664502726176fcdce0a398319
Kevin Brill <kevin.brill@gmail.com>
2014-12-26 20:43:39 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+var route = require('koa-route');
+var koa = require('koa');
+var bodyParser = require('koa-bodyparser');
+var mongo = require('co-mongo');
+
+var app = koa();
+
+function *priceByItem(id) {
+    var db = yield mongo.connect("mongodb://127.0.0.1:27017/bahralt");
+    var collection = yield db.collection('prices');
+
+    var docs = yield collection.find( {'id' : id }).toArray();
+
+    //var foo = (docs.length === 0 ) ? {} : docs.toArray();
+    this.body = docs;
+
+    yield db.close();
+}
+
+function *upload() {
+    console.log(this.request);
+    this.body = { status: 'upload' };
+}
+
+app.use(bodyParser());
+app.use(route.get('/api/v1/item/:id', priceByItem));
+app.use(route.post('/api/v1/upload', upload));
+
+var server = app.listen(3000);
