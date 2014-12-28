448245262a0d4c569d4dd93e8a449bef5c785f6a
Kevin Brill <kevin.brill@gmail.com>
2014-12-28 12:34:17 -0500

    Added ability to pull items based on shard

--- src/index.js
+++ src/index.js
@@ -11,10 +11,22 @@ function *priceByItem(id) {
 
     var docs = yield collection.find( {'id' : id }).toArray();
 
-    //var foo = (docs.length === 0 ) ? {} : docs.toArray();
     this.body = docs;
 
     yield db.close();
+}
+
+function *priceByItemByShard(id, shard) {
+
+    var db = yield mongo.connect("mongodb://127.0.0.1:27017/bahralt");
+    var collection = yield db.collection('prices');
+
+    var docs = yield collection.find( {'id' : id, 'shards.name': shard }).toArray();
+
+    this.body = docs;
+
+    yield db.close();
+
 }
 
 function *upload() {
@@ -24,6 +36,7 @@ function *upload() {
 
 app.use(bodyParser());
 app.use(route.get('/api/v1/item/:id', priceByItem));
+app.use(route.get('/api/v1/item/:id/shards/:shard', priceByItemByShard));
 app.use(route.post('/api/v1/upload', upload));
 
 var server = app.listen(3000);
